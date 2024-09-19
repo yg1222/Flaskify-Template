41e593d38d9be510fd1c73639af7253d9f492105
Yemi George <yemi@dunnoservicesinc.com>
2024-09-19 12:40:46 -0600

    fix indentation

--- boilerplate.py
+++ boilerplate.py
@@ -129,29 +129,29 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     """,
         "app/blueprints/auth.py": """from flask import Blueprint
-    auth = Blueprint('auth', __name__)
+auth = Blueprint('auth', __name__)
 
-    @auth.route('/login')
-    def login():
-        return "Login Page"
+@auth.route('/login')
+def login():
+    return "Login Page"
 
-    @auth.route('/signup')
-    def signup():
-        return "Signup Page"
+@auth.route('/signup')
+def signup():
+    return "Signup Page"
     """,
         "app/blueprints/billing.py": """from flask import Blueprint
-    billing = Blueprint('billing', __name__)
+billing = Blueprint('billing', __name__)
 
-    @billing.route('/billing')
-    def billing_page():
-        return "Billing Page"
+@billing.route('/billing')
+def billing_page():
+    return "Billing Page"
     """,
         "app/services/stripe_service.py": """import stripe
-    stripe.api_key = os.environ.get('STRIPE_API_KEY')
+stripe.api_key = os.environ.get('STRIPE_API_KEY')
 
-    def create_checkout_session():
-        # Stripe integration logic
-        pass
+def create_checkout_session():
+    # Stripe integration logic
+    pass
     """,
         "wsgi.py": """from app import app
 
@@ -161,97 +161,97 @@ if __name__ == "__main__":
     ".gitignore": ignores,
     ".dockerignore": ignores,
     "app/templates/base.html":"""<!DOCTYPE html>
-    <html lang="en">
-    <head>
-    {% block head %}
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- mobile metas -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-    <!-- site metas -->
-    <meta name="keywords" content="">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    
-        <!-- style css -->
-    <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='css/style.css') }}">
-    
-    <link rel="icon" href="{{ url_for('static', filename='media/favicon.ico') }}" type="image/gif" />
-    
-    <title>{% block title %}{% endblock %} - App</title>
-    {% endblock %}
-    </head>
+<html lang="en">
+<head>
+{% block head %}
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- mobile metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+<!-- site metas -->
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="author" content="">
 
-    <body>
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">My Flask App</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/login">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/signup">Signup</a>
-                        </li>
-                    </ul>
-                </div>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- style css -->
+<link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='css/style.css') }}">
+
+<link rel="icon" href="{{ url_for('static', filename='media/favicon.ico') }}" type="image/gif" />
+
+<title>{% block title %}{% endblock %} - App</title>
+{% endblock %}
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">My Flask App</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/signup">Signup</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
-        
-        <!-- Main Content -->
-    <div class="page-content" id="content">
-        {% block content %}{% endblock %}
-    </div>
-
-
-    <!--footer section start -->
-    <div class="footer_section layout_padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-sm-6">
-                <div class="footer_logo"><img src="#"></div>
-                <p class="dolor_text"></p>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                <h4 class="address_text">Legal</h4>
-                <a href="#"><p class="dolor_text">Privacy Policy</p></a>
-                <a href="#"><p class="dolor_text">Terms of service</p></a>
-                
-                </div>
-                <div class="col-lg-4 col-sm-12">
-                <h4 class="address_text">Contact</h4>
-                <a href="mailto:#><p class="dolor_text">name@email.com</p></a>
-                
-                </div>
-            </div>
-        
         </div>
-    </div>
-    <!--footer section end -->
-    <!-- copyright section start -->
-    <div class="copyright_section">
-        <div class="copyright_text">&copy; {{current_year}} CB App. All Rights Reserved.</div>
-    </div>
-    <!-- copyright section end -->
+    </nav>
+    
+    <!-- Main Content -->
+<div class="page-content" id="content">
+    {% block content %}{% endblock %}
+</div>
 
-    <!-- Javascript files-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="{{ url_for('static', filename='js/index.min.js') }}"></script>
 
-    </body>
-    </html>
+<!--footer section start -->
+<div class="footer_section layout_padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-sm-6">
+            <div class="footer_logo"><img src="#"></div>
+            <p class="dolor_text"></p>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+            <h4 class="address_text">Legal</h4>
+            <a href="#"><p class="dolor_text">Privacy Policy</p></a>
+            <a href="#"><p class="dolor_text">Terms of service</p></a>
+            
+            </div>
+            <div class="col-lg-4 col-sm-12">
+            <h4 class="address_text">Contact</h4>
+            <a href="mailto:#><p class="dolor_text">name@email.com</p></a>
+            
+            </div>
+        </div>
+    
+    </div>
+</div>
+<!--footer section end -->
+<!-- copyright section start -->
+<div class="copyright_section">
+    <div class="copyright_text">&copy; {{current_year}} CB App. All Rights Reserved.</div>
+</div>
+<!-- copyright section end -->
+
+<!-- Javascript files-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="{{ url_for('static', filename='js/index.min.js') }}"></script>
+
+</body>
+</html>
     """,
     ".env":"""SECRET_KEY=""
 STRIPE_API_KEY=""
@@ -262,13 +262,12 @@ MAIL_PASSWORD=""
 # MAIL_DEFAULT_SENDER=("Flask App", "x@x.com")
 # """,
     "requirements.txt":"""Flask
-    flask-blueprint
-    Flask-Mail
-    Flask-SQLAlchemy
-    python-dotenv
-    requests
-    SQLAlchemy""",
-
+flask-blueprint
+Flask-Mail
+Flask-SQLAlchemy
+python-dotenv
+requests
+SQLAlchemy""",
     }
 
     # Create the folder structure
